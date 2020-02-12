740715dc5b7aca4ba7ddb4bed5622f93e5675747
Bhagya Sree Chanda <bhagyasree.c60@gmail.com>
2020-02-12 15:12:30 -0600

    comitting after fixing issues

--- 1mapper.py
+++ 1mapper.py
@@ -2,7 +2,7 @@ input = open("purchases.txt", "r")
 output = open("01.txt", "w")
 
 for line in input:
-    datalist = line.strip().split("    ")
+    datalist = line.strip().split(" ")
     date, time,store, item, cost, paymentType = datalist
     output.write(paymentType + "\t" + "1" + "\n")
 
--- 3reducer.py
+++ 3reducer.py
@@ -3,18 +3,23 @@ r = open("03.txt", "w")
 
 thisKey = ""
 thisValue = 0.0
-count=0
+
+
 for line in s:
   
   data = line.strip().split('\t')
   paymentType, amount = data
   #print("count: ",count)
   if paymentType != thisKey:
+    count=0
     if thisKey:
       # output the last key value pair result
       r.write(thisKey + '\t' + str(thisValue)+'\n')
+      count+=1
+    else:
+      count+=1
       
-    count+=1  
+      
     # start over when changing keys
     thisKey = paymentType 
     thisValue = 0.0
